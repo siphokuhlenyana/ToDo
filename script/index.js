8b21ab48ed8aab4e23fe185c629de89315e539ab
Siphokuhle <siphokuhlenyana@gmail.com>
2024-06-07 15:54:06 +0200

    Added

---
+++ script/index.js
@@ -0,0 +1,36 @@
+
+
+let input =document.querySelectorAll('input')
+let add =document.querySelector('.add')
+let sort=document.querySelector('#sort')
+let results=document.querySelector('ul')
+
+
+
+
+// function addList(){
+//     input.value =
+// }
+
+
+// add.addEventListener('click',addList)
+
+function CreateList(id,name,createdDate,completed){
+    this.id=id 
+    this.name=name
+    this.createdDate=createdDate
+    this.complete
+
+
+}
+
+  
+let myList=[input.value]
+myList.forEach(item =>{
+    ul.innerHTML += `
+    
+    
+    
+    ` 
+})
+
